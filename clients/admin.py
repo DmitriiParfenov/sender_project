b3cd59915b5017ee2f618d7643d4a70c8eeb8c4d
Dmitrii <dima12sik@yandex.ru>
2023-07-26 15:51:57 +0300

    updated admin.py and apps.py

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from clients.models import Client
+
+
 # Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name', 'email')
+    list_display_links = ('first_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email')
