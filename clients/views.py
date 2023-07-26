409bb5684a1cc96aa4d33715fb66e45dcfd22db9
Dmitrii <dima12sik@yandex.ru>
2023-07-26 15:51:01 +0300

    updated models.py to the sender app

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DetailView, DeleteView
+
+from clients.models import Client
+
 
 # Create your views here.
+class SenderTemplateView(TemplateView):
+    template_name = 'clients/index.html'
+    extra_context = {
+        'title': 'Sender: Главная'
+    }
+
+
+class ClientListView(ListView):
+    model = Client
+    paginate_by = 3
+    extra_context = {
+        'title': 'Клиенты'
+    }
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('first_name', 'last_name', 'middle_name', 'email', 'comment')
+    success_url = reverse_lazy('client:clients_list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ('first_name', 'last_name', 'middle_name', 'email', 'comment')
+
+    def get_success_url(self):
+        return reverse('client:client_detail', args=[self.kwargs.get('pk')])
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('client:clients_list')
