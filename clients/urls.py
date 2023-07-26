2119a7cc13e87403fb02d5f95ca6bd29855af4ce
Dmitrii <dima12sik@yandex.ru>
2023-07-26 15:50:33 +0300

    add urls.py to the sender app

---
+++ clients/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from clients.apps import ClientsConfig
+from clients.views import SenderTemplateView, ClientListView, ClientCreateView, ClientUpdateView, ClientDetailView, ClientDeleteView
+
+app_name = ClientsConfig.name
+
+urlpatterns = [
+    path('', SenderTemplateView.as_view(), name='index'),
+    path('clients/', ClientListView.as_view(), name='clients_list'),
+    path('add_client/', ClientCreateView.as_view(), name='add_client'),
+    path('update_client/<int:pk>/', ClientUpdateView.as_view(), name='update_client'),
+    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
+    path('delete_client/<int:pk>/', ClientDeleteView.as_view(), name='delete_client')
+]
