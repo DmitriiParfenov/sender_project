33ef4d1cf928d1ac8239c5fdace908b10081c5ec
Dmitrii <dima12sik@yandex.ru>
2023-08-16 19:10:19 +0300

    registered Sender and SenderLog in the admin panel

--- sender/admin.py
+++ sender/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from sender.models import Sender, SenderLog
+
+
 # Register your models here.
+@admin.register(Sender)
+class SenderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'subject', 'message', 'period', 'status', 'time')
+    list_display_links = ('subject', )
+    search_fields = ('subject', 'client')
+    list_filter = ('subject', 'client')
+
+
+@admin.register(SenderLog)
+class SenderLogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'client', 'sender', 'status', 'last_try')
+    list_display_links = ('client', 'last_try')
+    search_fields = ('client', 'status')
+    list_filter = ('client', 'status')
