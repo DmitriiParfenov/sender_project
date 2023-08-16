9c40ac204fe3d8d4e875194a1b321ce5cddcfd43
Dmitrii <dima12sik@yandex.ru>
2023-08-16 19:09:53 +0300

    updated settings.py to send messages

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'clients.apps.ClientsConfig',
-
+    'sender.apps.SenderConfig',
+    'django_crontab',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,15 @@ MEDIA_ROOT = BASE_DIR / 'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_HOST_USER = os.getenv('yandex_login')
+EMAIL_HOST_PASSWORD = os.getenv('yandex_password_smtp')
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+CRONJOBS = [
+    ('* * * * *', 'sender.cron.my_scheduled_job')
+]
