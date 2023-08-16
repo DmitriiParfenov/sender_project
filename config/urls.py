5c0c37cf5e7288f86caf0cab9a02c0a00d6a286f
Dmitrii <dima12sik@yandex.ru>
2023-08-16 20:01:44 +0300

    add paths to all apps

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('clients.urls', namespace='client'))
+    path('', include('clients.urls', namespace='client')),
+    path('sender/', include('sender.urls', namespace='sender'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
