4b7beb571d677f6ec7061a4e16b2cdcf664a43e0
Dmitrii <dima12sik@yandex.ru>
2023-08-21 23:40:31 +0300

    cached SenderTemplateView

--- clients/urls.py
+++ clients/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from clients.apps import ClientsConfig
-from clients.views import SenderTemplateView, ClientListView, ClientCreateView, ClientUpdateView, ClientDetailView, ClientDeleteView
+from clients.views import SenderTemplateView, ClientListView, ClientCreateView, ClientUpdateView, ClientDetailView, \
+    ClientDeleteView
 
 app_name = ClientsConfig.name
 
 urlpatterns = [
-    path('', SenderTemplateView.as_view(), name='index'),
+    path('', cache_page(60)(SenderTemplateView.as_view()), name='index'),
     path('clients/', ClientListView.as_view(), name='clients_list'),
     path('add_client/', ClientCreateView.as_view(), name='add_client'),
     path('update_client/<int:pk>/', ClientUpdateView.as_view(), name='update_client'),
