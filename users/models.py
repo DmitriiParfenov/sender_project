7eebf6f149d267a50ab5c12f73a0fae1ab52ff42
Dmitrii <dima12sik@yandex.ru>
2023-08-21 23:23:57 +0300

    add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from clients.models import NULLABLE
+
+
 # Create your models here.
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=30, verbose_name='номер телефона', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.CharField(max_length=150, verbose_name='страна', **NULLABLE)
+    user_identity = models.CharField(max_length=20, verbose_name='ключ', **NULLABLE)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        permissions = [
+            (
+                'set_disabled',
+                'Can disable Пользователь'
+            )
+        ]
+
